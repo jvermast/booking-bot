b97a9145264c4fb5834bffc4f5b7aa8c4c70e169
jvermast <jvermast@gmail.com>
2021-12-06 11:52:36 -0500

    Update SiteBot.py

--- SiteBot.py
+++ SiteBot.py
@@ -9,21 +9,21 @@ from selenium.webdriver.common.by import By
 from tabulate import tabulate
 from datetime import datetime
 from random import randrange
+from contextlib import suppress
 import time
 import sys
 import pybase64
 import warnings
 import logging
-from contextlib import suppress
 
-
+#LOGGING
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
 
-username = "m"
+username = ""
 password = ""
 
 numcampers = 3
